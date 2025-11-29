6ed4f6ecbcd67e609055468a3ba6045bb7ba7290
malgorzatatanska <malgosiatanska1@gmail.com>
2025-11-29 23:31:56 +0100

    Homework Assignment - Flow Fundamentals part 2

--- app/src/main/java/com/plcoding/coroutinesmasterclass/util/CoundownScreen.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/util/CoundownScreen.kt
@@ -1,15 +1,21 @@
 package com.plcoding.coroutinesmasterclass.util
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,40 +26,69 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun CountdownScreen(
-    viewModel: CountdownViewModel = viewModel(), modifier: Modifier =
-        Modifier
+    viewModel: CountdownViewModel = viewModel(), modifier: Modifier = Modifier
 ) {
 
     val value by viewModel.count.collectAsStateWithLifecycle()
+    val enteredValue = viewModel.input
 
     Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center
     ) {
-        Column {
-            Text("Odliczanie ${value}")
-            Button(
-                onClick = {
-                    viewModel.start()
+        Column(modifier = Modifier.padding(20.dp)) {
+            Text("Wpisz wybrana warrtosc")
+            TextField(
+                onValueChange = { newValue: String ->
+                    val number = newValue.toIntOrNull() ?: 0
+                    viewModel.setCounter(number)
+
                 },
-                enabled = true,
-                modifier = Modifier.padding(20.dp)
+                value = enteredValue.toString(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = 20.dp,
+                        bottom = 20.dp
+                    )
+            )
+            Text("Odliczanie ${value}")
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment =
+                    Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
             ) {
-                Text("Start counting")
+                Button(
+                    onClick = {
+                        viewModel.start()
+                    }, enabled = true, modifier = Modifier.padding(20.dp)
+                ) {
+                    Text("Start counting")
+                }
+                Button(
+                    onClick = { viewModel.reset() },
+                    modifier = Modifier.padding(
+                        20.dp
+                    )
+                ) {
+                    Text("Wyzeruj")
+                }
             }
+
+
         }
+
     }
 }
 
 @Preview(
     showBackground = true, device = Devices.PIXEL_4, showSystemUi = true,
-    widthDp = 411,   // szerokość Pixel 4 (portrait)
-    heightDp = 891   // wysokość Pixel 4 (portrait)
 
-)
+
+    )
 @Composable
 fun CountdownScreenPreview() {
-    CountdownScreen(viewModel = CountdownViewModel())
+    CountdownScreen()
 }
 
 
--- app/src/main/java/com/plcoding/coroutinesmasterclass/util/view/CountdownViewModel.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/util/view/CountdownViewModel.kt
@@ -1,7 +1,13 @@
 package com.plcoding.coroutinesmasterclass.util.view
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,20 +17,47 @@ import kotlinx.coroutines.launch
 
 class CountdownViewModel : ViewModel() {
 
-    private val _count = MutableStateFlow(10);
+    private val _count = MutableStateFlow(0);
     val count = _count.asStateFlow();
+    private var countingJob: Job? = null
+
+    var input by mutableStateOf("")
+        private set
+
+
+    fun setCounter(value: Int) {
+        input = if (value <= 0) "" else value.toString()
+    }
 
     fun start() {
-        viewModelScope.launch {
+        countingJob?.cancel()
+
+        val startValue = input.toIntOrNull() ?: 0 // ← tutaj konwersja
+        _count.value = startValue
+        println("wartosc inputa: ${input}")
+
+
+        countingJob = viewModelScope.launch {
             countDownFlow().collect { value ->
                 _count.value = value
             }
         }
     }
 
+    fun reset() {
+        viewModelScope.launch {
+            countingJob?.cancel()
+            countingJob = null
+            _count.value = 0
+            input = ""
+
+        }
+
+    }
+
 
     fun countDownFlow(): Flow<Int> = flow {
-        for (i in 10 downTo 0) {
+        for (i in input.toInt() downTo 0) {
             emit(i)
             delay(1000)
         }
